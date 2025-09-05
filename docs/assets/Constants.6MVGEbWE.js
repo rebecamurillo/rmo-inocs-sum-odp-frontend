f4166ca9a34f83b85170c8d99a07aa262e4161a2
Rebeca MURILLO <rebeca.murillo.pro@gmail.com>
2025-09-05 13:23:40 +0200

    update static demo

---
+++ docs/assets/Constants.6MVGEbWE.js
@@ -0,0 +1,1 @@
+const O="#004494",C="#98c33a",s="#ff632f",R="#ff442f",_="#98c33a80",a="#ff442f80";export{s as C,_ as a,a as b,C as c,R as d,O as e};
---
+++ docs/assets/links.BishvqZb.js
@@ -0,0 +1,1 @@
+function o(n){return`/inocs-sum-odp-frontend${n??"#"}`}export{o as g};
